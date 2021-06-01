386e01a5615fa7734c454d5d1731582c14cbf20f
dianzikuangjie <zhouhaigang123@126.com>
2021-06-01 14:57:03 +0900

    创建了一个Demo的新文件

---
+++ Test01/src/Demo.java
@@ -0,0 +1,12 @@
+/**
+ * @author tom
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2021/6/1 14:53
+ * @Copyright © 株式会社多言语系统研究所
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("创建了一个新的文件");
+    }
+}
